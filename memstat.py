922ea9e35f342d9582125f9147e6043951df7c7d
Yuki Minoh <yukikaze.0511@gmail.com>
2022-03-13 01:27:23 +0900

    list append

--- memstat.py
+++ memstat.py
@@ -9,20 +9,20 @@ def make_report(device_tensor_stat: dict, verbose: bool = False, target_device: 
     show_reuse = verbose
     template_format = '{:<40s}{:>20s}{:>10s}'
     result = []
-    result.push(template_format.format('Element type', 'Size', 'Used MEM'))
+    result.append(template_format.format('Element type', 'Size', 'Used MEM'))
     for device, tensor_stats in device_tensor_stat.items():
         # By default, if the target_device is not specified,
         # print tensors on all devices
         if target_device is not None and device != target_device:
             continuresult += ('-' * LEN)
-        result.push('Storage on {}'.format(device))
+        result.append('Storage on {}'.format(device))
         total_mem = 0
         total_numel = 0
         for stat in tensor_stats:
             name, size, numel, mem = stat
             if not show_reuse:
                 name = name.split('(')[0]
-            result.push(template_format.format(
+            result.append(template_format.format(
                 str(name),
                 str(size),
                 readable_size(mem),
@@ -30,21 +30,21 @@ def make_report(device_tensor_stat: dict, verbose: bool = False, target_device: 
             total_mem += mem
             total_numel += numel
 
-        result.push('-'*LEN)
-        result.push('Total Tensors: {} \tUsed Memory: {}'.format(
+        result.append('-'*LEN)
+        result.append('Total Tensors: {} \tUsed Memory: {}'.format(
             total_numel, readable_size(total_mem),
         ))
 
         if device != torch.device('cpu'):
             with torch.cuda.device(device):
                 memory_allocated = torch.cuda.memory_allocated()
-            result.push('The allocated memory on {}: {}'.format(
+            result.append('The allocated memory on {}: {}'.format(
                 device, readable_size(memory_allocated),
             ))
             if memory_allocated != total_mem:
-                result.push('Memory differs due to the matrix alignment or'
+                result.append('Memory differs due to the matrix alignment or'
                         ' invisible gradient buffer tensors')
-        result.push('-'*LEN)
+        result.append('-'*LEN)
 
     return "\n".join(result)
 
